eb987d15198db5c26b4680e5e07ac02b52cce14f
sollo11 <78496768+sollo11@users.noreply.github.com>
2021-03-19 22:21:14 +0100

    $

---
+++ ash Disk.  1/debug.js
@@ -0,0 +1,21 @@
+import { d } from './machine.js';
+
+function unknownState(from, state) {
+  throw new Error(`Cannot transition from ${from} to unknown state: ${state}`);
+}
+
+d._create = function(current, states) {
+  if(!(current in states)) {
+    throw new Error(`Initial state [${current}] is not a known state.`);
+  }
+  for(let p in states) {
+    let state = states[p];
+    for(let [, candidates] of state.transitions) {
+      for(let {to} of candidates) {
+        if(!(to in states)) {
+          unknownState(p, to);
+        }
+      }
+    }
+  }
+};
